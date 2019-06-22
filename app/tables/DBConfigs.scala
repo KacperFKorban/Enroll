e21d964c33b4b2968ac96c74e836a6b30b4cf135
Kacper Korban <kacper.f.korban@gmail.com>
2019-06-22 19:38:04 +0200

    Added DB initialization

--- app/tables/DBConfigs.scala
+++ app/tables/DBConfigs.scala
@@ -1,6 +1,10 @@
 package tables
 
+import models.Course
 import slick.jdbc.PostgresProfile.api._
+import tables.Courses.courses
+import tables.Students.students
+import tables.StudentsToCourses.studentsToCourses
 
 object DBConfigs {
 
@@ -8,8 +12,18 @@ object DBConfigs {
 
 
   def main(args: Array[String]): Unit = {
-    val schema = Students.students.schema ++ Courses.courses.schema ++ StudentsToCourses.studentsToCourses.schema
-    db.run(schema.create)
+    val schema = students.schema ++ courses.schema ++ studentsToCourses.schema
+    db.run(DBIO.seq(
+      schema.create,
+      courses += Course(0, "Scala"),
+      courses += Course(1, "Erlang"),
+      courses += Course(2, "Python"),
+      courses += Course(3, "C++"),
+      courses += Course(4, "Assembler"),
+      courses += Course(5, "Fortran"),
+      courses += Course(6, "Ruby"),
+      courses += Course(7, "JavaScript"),
+    ))
     schema.create.statements.foreach(println(_))
     Thread.sleep(1000)
   }
